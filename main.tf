8a5aec3ed6d578a07499175ac957d5eb5cd37f81
James Kipngetich <jameskipngetich166@gmail.com>
2026-02-15 12:41:58 +0300

    Add IaC

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# s3 Bucket module
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "s3-uploader-321" #replace with your bucket name
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    name        = "learn_tf"
+    environment = "demo"
+  }
+}
